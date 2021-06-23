f420de4fdad68c3c5f9ad425e3cf201010f321c9
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-06-24 00:03:34 +0200

    add app to installer

--- Admin/Installer.php
+++ Admin/Installer.php
@@ -14,11 +14,14 @@ declare(strict_types=1);
 
 namespace Modules\CMS\Admin;
 
-use Modules\CMS\Models\Application;
-use Modules\CMS\Models\ApplicationMapper;
+use phpOMS\Module\InstallerAbstract;
 use phpOMS\Message\Http\HttpRequest;
 use phpOMS\Message\Http\HttpResponse;
-use phpOMS\Module\InstallerAbstract;
+use Modules\CMS\Models\Application;
+use Modules\CMS\Models\ApplicationMapper;
+use phpOMS\Application\ApplicationAbstract;
+use phpOMS\System\File\PathException;
+use phpOMS\Uri\HttpUri;
 
 /**
  * Installer class.
@@ -30,7 +33,7 @@ use phpOMS\Module\InstallerAbstract;
  */
 final class Installer extends InstallerAbstract
 {
-    /**
+	/**
      * Install data from providing modules.
      *
      * @param DatabasePool $dbPool Database pool
@@ -40,7 +43,7 @@ final class Installer extends InstallerAbstract
      *
      * @since 1.0.0
      */
-    public static function installExternal(DatabasePool $dbPool, array $data) : array
+    public static function installExternal(ApplicationAbstract $app, array $data) : array
     {
     	if (!\is_file($data['path'] ?? '')) {
             throw new PathException($data['path'] ?? '');
@@ -56,22 +59,22 @@ final class Installer extends InstallerAbstract
             throw new \Exception(); // @codeCoverageIgnore
         }
 
-    	if (!isset($cmsData['name'], $cmsData['src'], $cmsData['dest'])) {
+    	if (!isset($cmsData[0]['name'], $cmsData[0]['src'], $cmsData[0]['dest'])) {
     		return [];
     	}
 
         $response = new HttpResponse();
-		$request        = new HttpRequest(new HttpUri(''));
+		$request  = new HttpRequest(new HttpUri(''));
 
 		$request->header->account = 1;
-		$request->setData('name', $cmsData['name']);
-        $request->setData('appSrc', $cmsData['src']);
-        $request->setData('appDest', $cmsData['dest']);
-        $request->setData('theme', $cmsData['theme'] ?? 'Default', true);
-        \Modules\Admin\Controller\ApiController::apiInstallApplication($request, $response, []);
+		$request->setData('name', $cmsData[0]['name']);
+        $request->setData('appSrc', $cmsData[0]['src']);
+        $request->setData('appDest', $cmsData[0]['dest']);
+        $request->setData('theme', $cmsData[0]['theme'] ?? 'Default', true);
+        $app->moduleManager->get('Admin')->apiInstallApplication($request, $response);
 
         $app       = new Application();
-        $app->name = $cmsData['name'];
+        $app->name = $cmsData[0]['name'];
 
         ApplicationMapper::create($app);
 
