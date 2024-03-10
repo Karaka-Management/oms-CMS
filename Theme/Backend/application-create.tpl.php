b090a34a65f2a5d832cd2ee9dd5f18caed2a5e07
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2024-03-10 02:24:56 +0000

    bump

--- Theme/Backend/application-create.tpl.php
+++ Theme/Backend/application-create.tpl.php
@@ -21,16 +21,24 @@ echo $this->data['nav']->render(); ?>
             <form id="cms-application-create" action="<?= \phpOMS\Uri\UriFactory::build('{/api}cms/application/upload'); ?>" method="PUT">
                 <div class="portlet-head"><?= $this->getHtml('Application'); ?></div>
                 <div class="portlet-body">
-                    <table class="layout wf-100" style="table-layout: fixed">
-                        <tbody>
-                        <tr><td><label for="iTitle"><?= $this->getHtml('Title'); ?></label>
-                        <tr><td><input id="iTitle" name="name" type="text" required>
-                        <tr><td><label for="iFile"><?= $this->getHtml('File'); ?></label>
-                        <tr><td><input id="iFile" name="files" type="file" required>
-                        <tr><td><label for="iDescription"><?= $this->getHtml('Description'); ?></label>
-                        <tr><td><?= $this->getData('editor')->render('report-editor'); ?>
-                        <tr><td><?= $this->getData('editor')->getData('text')->render('report-editor', 'description', 'cms-application-create'); ?>
-                    </table>
+                    <div class="form-group">
+                        <label for="iTitle"><?= $this->getHtml('Title'); ?></label>
+                        <input id="iTitle" name="name" type="text" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="iFile"><?= $this->getHtml('File'); ?></label>
+                        <input id="iFile" name="files" type="file" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="iDescription"><?= $this->getHtml('Description'); ?></label>
+                        <?= $this->getData('editor')->render('report-editor'); ?>
+                    </div>
+
+                    <div class="form-group">
+                        <?= $this->getData('editor')->getData('text')->render('report-editor', 'description', 'cms-application-create'); ?>
+                    </div>
                 </div>
                 <div class="portlet-foot">
                     <input type="submit" id="iApplicationCreateButton" name="applicationCreateButton" value="<?= $this->getHtml('Create', '0', '0'); ?>">
